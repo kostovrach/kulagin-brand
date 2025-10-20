08c13faf22828d847825eb7c8ae9b1ecd0beb2e4
kostovrach <merkulovich166@gmail.com>
2025-10-20 15:55:09 +0300

    delete image processing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
-import legacy from '@vitejs/plugin-legacy';
+// import legacy from '@vitejs/plugin-legacy';
 import { terser } from 'rollup-plugin-terser';
-import viteImagemin from 'vite-plugin-imagemin';
-import viteCompression from 'vite-plugin-compression';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+// import viteImagemin from 'vite-plugin-imagemin';
+// import viteCompression from 'vite-plugin-compression';
+// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import cssnano from 'cssnano';
 
 const rootDir = fileURLToPath(new URL('.', import.meta.url));
@@ -54,38 +54,29 @@ export default defineConfig({
             inject: 'body-last',
         }),
 
-        legacy({
-            targets: ['defaults', 'not IE 11'],
-        }),
+        // legacy({
+        //     targets: ['defaults', 'not IE 11'],
+        // }),
 
-        ViteImageOptimizer({
-            jpg: { quality: 85 },
-            png: { quality: 85 },
-            gif: { optimizationLevel: 3 },
-        }),
+        // ViteImageOptimizer({
+        //     jpg: { quality: 85 },
+        //     png: { quality: 85 },
+        //     gif: { optimizationLevel: 3 },
+        // }),
 
-        viteImagemin({
-            gifsicle: { optimizationLevel: 3 },
-            optipng: { optimizationLevel: 5 },
-            mozjpeg: { quality: 85 },
-            svgo: true,
-        }),
+        // viteImagemin({
+        //     gifsicle: { optimizationLevel: 3 },
+        //     optipng: { optimizationLevel: 5 },
+        //     mozjpeg: { quality: 85 },
+        //     svgo: true,
+        // }),
 
-        viteCompression({ algorithm: 'gzip' }),
-        viteCompression({ algorithm: 'brotliCompress' }),
+        // viteCompression({ algorithm: 'gzip' }),
+        // viteCompression({ algorithm: 'brotliCompress' }),
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
-    // server: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://localhost:8055',
-    //             changeOrigin: true,
-    //             rewrite: (path) => path.replace(/^\/api/, ''),
-    //         },
-    //     },
-    // },
 });
